d4d0b69dab0009fe878c6a3a7ab082980717ca97
Juan José <correopersonal436@gmail.com>
2022-05-30 00:05:49 +0200

    Final Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Twitter Sentiment Analysis using NLTK libray
+
+Development of an machine learning model with natural language processing technologies capable of detecting the type of sentiment (positive or negative) in Twitter texts using NLTK library for Python
+
+For be able to run the the Jupyter Notebook of the project you must have the necesary packages installed, for that you need to insert the following command in your computer's console `pip install -r requirements.txt`
+
+![Image](data/image.png)
--- models.py
+++ models.py
@@ -8,24 +8,37 @@ class CleanData():
     def __init__(self):
         pass
 
+
     def clean_emojis(self, text): # Clean emojis from the text
+
         regrex_pattern = re.compile(pattern = "["
+
             u"\U0001F600-\U0001F64F"  # emoticons
+
             u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+
             u"\U0001F680-\U0001F6FF"  # transport & map symbols
+
             u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+
             u"\U0001F900-\U0001F9FF"  # supplemental symbols and pictographs
+
             u"\u2764"
+
             u"\u2661"                 
                                 "]+", flags = re.UNICODE)
+
         return regrex_pattern.sub(r"", text)
 
+
     def remove_doubles(self, text):
+
         text = re.sub(r"(.)\1+", r"\1\1", text) # Convert more than 2 letter repetitions to 2 letter --- funnnnny --> funny
 
         text = re.sub(r"(-|\')", "", text) # Remove - & '
 
         return text
+
 
     def clean_laughs(self, text):
         text = re.sub(r"(jaja*|jeje*)", "", text.lower()) # Remove laughs
